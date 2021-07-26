e1cd4b7b22f412f23581279bcef0c3ed063c7e1a
Pratik Jha <Pratikgns@gmail.com>
2021-07-26 20:34:04 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+const navtoggle=document.querySelector('.nav-toggle');
+const navlinks=document.querySelectorAll('.navlink');
+
+navtoggle.addEventListener('click',()=>{
+    document.body.classList.toggle('nav-open');
+});
+
+navlinks.forEach(link =>{
+    link.addEventListener('click',()=>{
+        document.body.classList.remove('nav-open');})
+})
